17fcc6e5a2b3c14750c38cb49e33613c981d3eee
Fabrizio <fabrizio@kalimaha-fao.(none)>
2014-07-22 12:12:55 +0200

    LanguageExpression

--- js/scripts/component/core/balanceSheet/views/gridDataView/GridDataView.js
+++ js/scripts/component/core/balanceSheet/views/gridDataView/GridDataView.js
@@ -10,7 +10,7 @@ define(["jquery" , "infragistics"], function ($, pivot) {
     }
 
 
-    GridDataView.prototype.init = function (Configuration, gridModel, tableModel,configurator, typeOfCreation) {
+    GridDataView.prototype.init = function (Configuration, gridModel, tableModel, configurator, typeOfCreation) {
 
         console.log("GridDataView")
         console.log(Configuration)
@@ -31,6 +31,7 @@ define(["jquery" , "infragistics"], function ($, pivot) {
         getItem = function (propertyName) {
 
             return function (items, cellMetadata) {
+                debugger;
 
                 $.each(items, function (index, item) {
                     sum = item[propertyName];
@@ -40,14 +41,13 @@ define(["jquery" , "infragistics"], function ($, pivot) {
         }
 
 
-
-        var fullModel         = Configurator.getAllColumnModels();
+        var fullModel = Configurator.getAllColumnModels();
         var configurationKeys = Configurator.getKeyColumnConfiguration();
-        accessorMap           = Configurator.getAccessorMap();
-        var leftDimensions =    this.createLeftPivotDimension(fullModel["leftColumnsModel"], configurationKeys);
-        var upDimensions =      this.createUpPivotDimension(fullModel["upColumnsModel"], configurationKeys);
+        accessorMap = Configurator.getAccessorMap();
+        var leftDimensions = this.createLeftPivotDimension(fullModel["leftColumnsModel"], configurationKeys);
+        var upDimensions = this.createUpPivotDimension(fullModel["upColumnsModel"], configurationKeys);
 
-    //  var measuresDimension = this.createMeasureDimension(fullModel["valueColumnsModel"], fullModel["accessorColumnsModel"])
+        //  var measuresDimension = this.createMeasureDimension(fullModel["valueColumnsModel"], fullModel["accessorColumnsModel"])
 
         var dataSource = new $.ig.OlapFlatDataSource({
             dataSource: table,
@@ -58,29 +58,27 @@ define(["jquery" , "infragistics"], function ($, pivot) {
                     measuresDimension: {
                         caption: "Measures",
                         measures: [ //for each measure, name and aggregator are required
-                            { caption: "value", name: "value", aggregator: getItem(4) }
+                            { caption: "value", name: "value", aggregator: this.expressionLanguage() }
                         ]
                     },
                     dimensions: [ // for each dimension
                         {
                             // For each dimension at least one hierarchy must be defined.
-                            caption: "Rows", name: "Rows", /*displayFolder: "Folder1\\Folder2",*/ hierarchies: leftDimensions
+                            caption: "Rows", name: "Rows", hierarchies: leftDimensions
                         },
                         {
-                            caption: "Columns", name: "Columns", /*displayFolder: "Folder1\\Folder2",*/ hierarchies:upDimensions
+                            caption: "Columns", name: "Columns", displayFolder: "Folder1\\Folder2", hierarchies: upDimensions
                         }
                     ]
 
                 }
             },
             // Preload hiearhies for the rows, columns, filters and measures
-            rows: "[Rows].["+titlesLeft[0]+"],[Rows].["+titlesLeft[1]+"]",
-            columns: "[Columns].["+titlesUp[0]+"],[Columns].["+titlesUp[1]+"]",
+            rows: "[Rows].[" + titlesLeft[0] + "],[Rows].[" + titlesLeft[1] + "]",
+            columns: "[Columns].[" + titlesUp[0] + "],[Columns].[" + titlesUp[1] + "]",
             measures: "[Measures].[value]"
 
         });
-
-        debugger;
 
 
         $("#pivotGrid").igPivotGrid({
@@ -90,88 +88,106 @@ define(["jquery" , "infragistics"], function ($, pivot) {
             compactHeaderIndentation: 80,
             isParentInFrontForColumns: true,
             gridOptions: {
-                caption: "Compact Layout",
-                defaultColumnWidth: 150
-
+                defaultColumnWidth: 100
             },
             width: "100%",
             height: "100%"
 
 
         });
-        var grid=$("#pivotGrid").igPivotGrid("grid");
+        var grid = $("#pivotGrid").igPivotGrid("grid");
 
-        $(document).delegate("#"+grid.id(), "iggridcellclick", function (evt, ui) {
+        $(document).delegate("#" + grid.id(), "iggridcellclick", function (evt, ui) {
             // Only the FIRST ROW column indexes start from 2, it needs to be checked!
-            alert("Cell Clicked. Cell at row index:"+ ui.rowIndex+"  and column index: "+  ui.colIndex);
-            ui.cellElement.innerText  ="BBB"
+            alert("Cell Clicked. Cell at row index:" + ui.rowIndex + "  and column index: " + ui.colIndex);
+            ui.cellElement.innerText = "BBB"
 
         });
     }
 
 
-    GridDataView.prototype.setPropertiesDatasource = function(){
+    GridDataView.prototype.expressionLanguage = function(){
+        var value =300;
+        var UM = "KG"
+        var conditionRegExpression = /(#(\w+)(\W))/;
+        var valuesRegExpression    = /(((\w)|(\W))*(\$\w+)((\w)|(\W))*(\~))/;
+
+
+        return function (items, cellMetadata) {
+            var result = "X "
+            $.each(items, function (index, item) {
+
+                var expression = "#value|$value~#UM| ($UM) ||";
+                var firstCondition = expression.match(conditionRegExpression)[0]
+                expression = expression.replace(conditionRegExpression, "")
+                firstCondition = firstCondition.slice(0, -1);
+                if(firstCondition.substring(0,1) == "#"){
+                    if(firstCondition.substring(1) == "value"  && typeof item[4] !== 'undefined'){
+                    result += item[4];
+                    var secondCondition = expression.match(valuesRegExpression)[0];
+                        debugger;
+
+                    }
+                }
+
+            })
+            return result;
+        }
+    }
+
+
+    GridDataView.prototype.setPropertiesDatasource = function () {
 
         var result = {};
-        result["rows"] = "[Rows].["+titlesLeft[0]+"]";
-        if(titlesLeft.length >1){
-            result["rows"] += ",[Rows].["+titlesLeft[1]+"]";
+        result["rows"] = "[Rows].[" + titlesLeft[0] + "]";
+        if (titlesLeft.length > 1) {
+            result["rows"] += ",[Rows].[" + titlesLeft[1] + "]";
         }
-        result["Columns"] = "[Columns].["+titlesUp[0]+"]";
-        if(titlesLeft.length >1){
-            result["Columns"] += ",[Columns].["+titlesUp[1]+"]";
+        result["Columns"] = "[Columns].[" + titlesUp[0] + "]";
+        if (titlesLeft.length > 1) {
+            result["Columns"] += ",[Columns].[" + titlesUp[1] + "]";
         }
 
-        result["Measures"] =   "[Measures].[value]"
+        result["Measures"] = "[Measures].[value]"
 
         return result;
 
     }
 
 
-    GridDataView.prototype.createLeftPivotDimension = function(keyColumns,keyColumnConf){
+    GridDataView.prototype.createLeftPivotDimension = function (keyColumns, keyColumnConf) {
 
         var that = this;
         titlesLeft = [];
         var keysLeft = [];
-        titlesLeft.push( keyColumns["leftColumns"][0].domain.title.EN)
-            var label = keyColumnConf["lefKeyColumnConfiguration"][0].properties.cellProperties.label
-            var key = {
-                caption :     keyColumns["leftColumns"][0].domain.title.EN,
-                name    :     keyColumns["leftColumns"][0].domain.title.EN,
-                levels  :    [
-                    {
-                        name:    keyColumns["leftColumns"][0].domain.supplemental.EN,
-                        caption: keyColumns["leftColumns"][0].domain.title.EN,
-                        memberProvider : function(item){
-                            var plus;
-                            var r = item[5]
-                                if(r){
-                                    plus = r;
-                                }
-                                else{
-                                    plus = "";
-                                }
-                            return item[0] + plus
-
-                            // return that.evaluateRegExpression(item, 0, keyColumnConf["lefKeyColumnConfiguration"][0], keyColumns["leftKeyIndexes"])
-                        }
-
+        titlesLeft.push(keyColumns["leftColumns"][0].domain.title.EN)
+        var label = keyColumnConf["lefKeyColumnConfiguration"][0].properties.cellProperties.label
+        var key = {
+            caption: keyColumns["leftColumns"][0].domain.title.EN,
+            name: keyColumns["leftColumns"][0].domain.title.EN,
+            levels: [
+                {
+                    name: keyColumns["leftColumns"][0].domain.supplemental.EN,
+                    caption: keyColumns["leftColumns"][0].domain.title.EN,
+                    memberProvider: function (item) {
+                        return  item[keyColumns["leftKeyIndexes"][0]]
                     }
-                ]
-            }
+
+                }
+            ]
+        }
         keysLeft.push(key);
-        if(keyColumns["leftColumns"].length >1){
+        if (keyColumns["leftColumns"].length > 1) {
             titlesLeft.push(keyColumns["leftColumns"][1].domain.title.EN)
             var key2 = {
-                caption :    keyColumns["leftColumns"][1].domain.title.EN,
-                name    :    keyColumns["leftColumns"][1].domain.title.EN,
-                levels  :    [
+                caption: keyColumns["leftColumns"][1].domain.title.EN,
+                name: keyColumns["leftColumns"][1].domain.title.EN,
+                levels: [
                     {
-                        name:    keyColumns["leftColumns"][1].domain.supplemental.EN,
+                        name: keyColumns["leftColumns"][1].domain.supplemental.EN,
                         caption: keyColumns["leftColumns"][1].domain.title.EN,
                         memberProvider: function (item) {
-                            return that.evaluateRegExpression2(item, 1, keyColumnConf["lefKeyColumnConfiguration"][0], keyColumns["leftKeyIndexes"])
+                            return  item[keyColumns["leftKeyIndexes"][1]]
                         }
                     }
                 ]
@@ -181,7 +197,7 @@ define(["jquery" , "infragistics"], function ($, pivot) {
         return keysLeft;
     }
 
-    GridDataView.prototype.evaluateRegExpression2 = function(item, num, configuration, keyIndexes){
+    GridDataView.prototype.evaluateRegExpression2 = function (item, num, configuration, keyIndexes) {
         var result;
         //   /\$\w+/  trova la prima $label
 
@@ -193,16 +209,16 @@ define(["jquery" , "infragistics"], function ($, pivot) {
         if (label == "$value") {
             array = label.match(/(\$\w+)/);
 
-        }else{
+        } else {
             array = label.match(/(\$\w+)(\s)(\W+)(\s*)(\$\w+)(\s*)(\W+)+/);
         }
         console.log(array)
         result = "";
-        for(var i = 1; i<array.length; i++){
-            if(array[i].substring(0,1) == "$"){
-                var value= array[i].substring(1)
-                result += (value == "value")? item[keyIndexes[num]]: item[accessorMap[value]];
-            } else{
+        for (var i = 1; i < array.length; i++) {
+            if (array[i].substring(0, 1) == "$") {
+                var value = array[i].substring(1)
+                result += (value == "value") ? item[keyIndexes[num]] : item[accessorMap[value]];
+            } else {
                 result += array[i];
             }
         }
@@ -210,7 +226,7 @@ define(["jquery" , "infragistics"], function ($, pivot) {
     }
 
 
-    GridDataView.prototype.evaluateRegExpression = function(item, num, configuration, keyIndexes){
+    GridDataView.prototype.evaluateRegExpression = function (item, num, configuration, keyIndexes) {
         var result;
         //   /\$\w+/  trova la prima $label
 
@@ -218,43 +234,22 @@ define(["jquery" , "infragistics"], function ($, pivot) {
 
         // start from array[1]
         var array;
-
-        debugger;
-        /*var result = label.replace(/(\$\w+)/g, function(firstParameter){
-            if (firstParameter == "$value") {
-                console.log(firstParameter + "VALUEEE");
-                console.log(item[keyIndexes[num]])
-              return  item[keyIndexes[num]]
-            }else{
-                debugger;
-                console.log(firstParameter + "ELSEE");
-                console.log(accessorMap)
-                console.log(item[accessorMap[firstParameter.substring(1)]])
-               return item[accessorMap[firstParameter.substring(1)]]
-            }
-        })
-        console.log("-------------------")
-
-        console.log(result)
-        console.log("-------------------")
-
-        */debugger;
         console.log("label!!")
         console.log(label)
         if (label == "$value") {
             array = label.match(/(\$\w+)/);
 
-        }else{
+        } else {
             array = label.match(/(\$\w+)(\s)(\W+)(\s*)(\$\w+)(\s*)(\W+)+/);
         }
-       // console.log(array)
+        // console.log(array)
         result = "";
-        for(var i = 1; i<array.length; i++){
-            if(array[i].substring(0,1) == "$"){
+        for (var i = 1; i < array.length; i++) {
+            if (array[i].substring(0, 1) == "$") {
 
-                var value= array[i].substring(1)
-                result += (value == "value")? item[keyIndexes[num]]: item[accessorMap[value]];
-            } else{
+                var value = array[i].substring(1)
+                result += (value == "value") ? item[keyIndexes[num]] : item[accessorMap[value]];
+            } else {
                 result += array[i];
             }
         }
@@ -262,57 +257,55 @@ define(["jquery" , "infragistics"], function ($, pivot) {
     }
 
 
-    GridDataView.prototype.createMeasureDimension = function(fullModel){
+    GridDataView.prototype.createMeasureDimension = function (fullModel) {
 
         var result = {};
-       var result1 = { caption: "value", name: "value", aggregator: getItem(4) }
+        var result1 = { caption: "value", name: "value", aggregator: getItem(4) }
         var indexValue = fullModel["valueColumnsModel"];
         var accessorModel = fullModel["accessorColumnsModel"]
         var measures = [];
-
-
 
 
         return measuresDimension;
     }
 
 
-    GridDataView.prototype.createUpPivotDimension = function(keyColumns,keyColumnConf){
+    GridDataView.prototype.createUpPivotDimension = function (keyColumns, keyColumnConf) {
 
         var that = this;
         titlesUp = []
-        titlesUp.push( keyColumns["upColumns"][0].domain.title.EN);
+        titlesUp.push(keyColumns["upColumns"][0].domain.title.EN);
 
         var keysUp = [];
-            var key = {
-                caption :     keyColumns["upColumns"][0].domain.title.EN,
-                name    :     keyColumns["upColumns"][0].domain.title.EN,
-                levels  :    [
+        var key = {
+            caption: keyColumns["upColumns"][0].domain.title.EN,
+            name: keyColumns["upColumns"][0].domain.title.EN,
+            levels: [
+                {
+                    name: keyColumns["upColumns"][0].domain.supplemental.EN,
+                    caption: keyColumns["upColumns"][0].domain.title.EN,
+                    memberProvider: function (item) {
+                        return  item[keyColumns["upKeyIndexes"][0]]
+                    }
+                }
+            ]}
+        keysUp.push(key);
+        if (keyColumns["upColumns"].length > 1) {
+            titlesUp.push(keyColumns["upColumns"][1].domain.title.EN)
+            var key2 = {
+                caption: keyColumns["upColumns"][1].domain.title.EN,
+                name: keyColumns["upColumns"][1].domain.title.EN,
+                levels: [
                     {
-                        name:    keyColumns["upColumns"][0].domain.supplemental.EN,
-                        caption: keyColumns["upColumns"][0].domain.title.EN,
+                        name: keyColumns["upColumns"][1].domain.supplemental.EN,
+                        caption: keyColumns["upColumns"][1].domain.title.EN,
                         memberProvider: function (item) {
-                            return that.evaluateRegExpression(item, 0, keyColumnConf["upKeyColumnConfiguration"][0], keyColumns["upKeyIndexes"])
+                            return  item[keyColumns["upKeyIndexes"][1]]
                         }
                     }
                 ]}
-            keysUp.push(key);
-            if( keyColumns["upColumns"].length >1){
-                titlesUp.push(keyColumns["upColumns"][1].domain.title.EN)
-                var key2 = {
-                    caption :    keyColumns["upColumns"][1].domain.title.EN,
-                    name    :    keyColumns["upColumns"][1].domain.title.EN,
-                    levels  :    [
-                        {
-                            name:    keyColumns["upColumns"][1].domain.supplemental.EN,
-                            caption: keyColumns["upColumns"][1].domain.title.EN,
-                            memberProvider: function (item) {
-                                return that.evaluateRegExpression(item, 1, keyColumnConf["upKeyColumnConfiguration"][1], keyColumns["upKeyIndexes"])
-                            }
-                        }
-                    ]}
-                keysUp.push(key2);
-            }
+            keysUp.push(key2);
+        }
         return keysUp;
     }
 
